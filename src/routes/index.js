bc6ce49ace7ef03ceff627e4223964d75af478fa
binhco1999 <tranbinhco99@gmail.com>
2022-05-25 11:52:29 +0700

    create router/layout for project

---
+++ src/routes/index.js
@@ -0,0 +1,17 @@
+import Home from '~/pages/Home';
+import Following from '~/pages/Following';
+import Profile from '~/pages/Profile';
+import Upload from '~/pages/Upload';
+import Search from '~/pages/Search';
+import { HeaderOnly } from '~/components/Layout';
+//không cần đăng nhập cũng dùng được
+const publicRoutes = [
+    { path: '/', component: Home },
+    { path: '/following', component: Following },
+    { path: '/profile', component: Profile },
+    { path: '/upload', component: Upload, layout: HeaderOnly },
+    { path: '/search', component: Search, layout: null },
+];
+//phải đăng nhập mới dùng được
+const privateRoutes = [];
+export { publicRoutes, privateRoutes };
